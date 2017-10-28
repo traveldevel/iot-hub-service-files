696dc1333814c3c78f50fba312b4b2ea5ee3250b
Travel Devel <traveldevel@hotmail.com>
2017-10-28 10:18:27 +0300

    upload for file is working

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var Grid = require('gridfs-stream');
 var fs = require('fs');
 const cfenv = require("cfenv");
 const basicAuth = require('basic-auth');
+var MongoClient = require('mongodb').MongoClient;
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
@@ -62,9 +63,14 @@ var getMongoUrlForService = function(mongoServiceName) {
     if(mongoService !== undefined){
         mongoCredentials = services[mongoServiceName].credentials;
         mongoUrl = mongoCredentials.uri;
+
+        var mongodbUri = require('mongodb-uri');
+        var uriObject = mongodbUri.parse(mongoUrl);
+        mongoDbName = uriObject.database;
         
         console.log("'" + mongoServiceName + "' found in VCAP_SERVICES ! ");
-        console.log("Url for mongodb : '" + mongoUrl + "'");        
+        console.log("Url for mongodb : '" + mongoUrl + "'");      
+        console.log("DB for mongodb : '" + mongoDbName + "'");  
     }
 
     return mongoUrl;
@@ -74,36 +80,25 @@ const landscapeName = process.env.LANDSCAPE_NAME;
 const tenantName = process.env.TENANT_NAME;
 
 const mongoServiceBaseName = "iot_hub_mongo_" + landscapeName + "_" + tenantName;
-const mongoUrlFiles = getMongoUrlForService(mongoServiceBaseName + "_files");
+var mongoConnData = getMongoUrlForService(mongoServiceBaseName + "_files");
+var mongoUrl = mongoConnData.url; 
+var mongoDbName = mongoConnData.db;
 
-if(mongoUrlFiles.length === 0){
+if(process.env.MONGODB_URL !== undefined && services[mongoServiceBaseName + "_files"] === undefined){
+    mongoUrl = process.env.MONGODB_URL;
+    var mongodbUri = require('mongodb-uri');
+    var uriObject = mongodbUri.parse(mongoUrl);
+    mongoDbName = uriObject.database;
+
+    console.log("mongodb url found in process.env.MONGODB_URL !");
+    console.log("Url for mongodb : '" + mongoUrl + "'");
+    console.log("DB for mongodb : '" + mongoDbName + "'");
+}
+
+if(mongoUrl.length === 0){
     console.log('No mongo files service Binded. Exiting...');
     return;
 }
-
-// create or use an existing mongodb-native db instance.
-// for this example we'll just create one:
-// var db = new mongodb.Db(mongoUrlFiles);
-
-// // make sure the db instance is open before passing into `Grid`
-// db.open(function (err) {
-//   if (err) return handleError(err);
-//   var gfs = Grid(db, mongodb);
-
-//   // all set!
-// })
-
-var MongoClient = require('mongodb').MongoClient;
-
-MongoClient.connect(mongoUrlFiles, function(err, db) {
-    
-    if (err) return handleError(err);
-
-    console.log("Connected correctly to server");
-
-    db.close();
-});
-
 
 var app = express();
 
@@ -135,7 +130,6 @@ app.get('/file/delete/:id', auth, function(req, res){
     });
 });
 
-
 //file download
 app.get('/file/download/:id', auth, function(req, res){
 
@@ -161,25 +155,62 @@ app.get('/file/download/:id', auth, function(req, res){
 });
 
 // file upload
-app.all('/file/upload', auth, function(req, res){
+app.all('/file/upload/:device_id', auth, function(req, res){
 
-    var dirname = require('path').dirname(__dirname);
-    var filename = req.files.file.name;
-    var path = req.files.file.path;
-    var type = req.files.file.mimetype;
+    var dirname = require('path').dirname(require.main.filename);
+    console.log(req.files);
+
+    var device_id = req.param('device_id');
+
+    var file = req.files[0];
+
+    var originalname = file.originalname;
+    var filename = file.filename;
+    var path = file.path;
+    var type = file.mimetype;
      
     var read_stream = fs.createReadStream(dirname + '/' + path);
      
-    var writestream = gfs.createWriteStream({
-       filename: filename
-    });
+    MongoClient.connect(mongoUrl, function(err, db) {
+        
+        if(err){
+            console.log(err);
+        }
+    
+        var gfs = Grid(db, mongodb);
 
-    read_stream.pipe(writestream);
+        var writestream = gfs.createWriteStream({
+            filename: filename
+        });
+     
+        read_stream.pipe(writestream);
+     
+        var completeName = dirname + '/uploads/' + filename;
 
-    var completeName = 'uploads/' + filename;
+        writestream.on('close', function(data) {
+            
+            console.log(data);
 
-    writestream.on('close', function () {
-        gfs.unlinkSync(completeName); 
+            var newFile = {
+                device_id: device_id,
+                gfs_id : data._id,
+                content_type: data.contentType,
+                md5: data.md5,
+                created_at: data.uploadDate,
+                size: data.length
+            };
+
+            db.collection("file").insertOne(newFile, function(err, result) {
+                if (err) throw err;
+
+                console.log("1 file inserted");
+                db.close();
+
+                fs.unlink(completeName, function(err) {
+                    res.send("OK");
+                });
+            });
+         });        
     });
 });
 
@@ -187,8 +218,8 @@ app.get('/', auth, function(req, res){
 
     res.writeHead(200, {'Content-Type': 'text/html'});
     
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<form action="file/upload/test_device_for_upload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br/>');
     res.write('<input type="submit">');
     res.write('</form>');
     
